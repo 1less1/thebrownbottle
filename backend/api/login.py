93c82e8d0496eb9f7c2f9b80de837a0d1d4a9783
1less1 <olhava.dom@gmail.com>
2025-05-01 09:49:05 -0500

    Added blueprint for login route in main.py

---
+++ backend/api/login.py
@@ -0,0 +1,9 @@
+from flask import jsonify
+import mysql.connector
+import os
+import request_helper
+
+# Future Login Function - /login route already added to main.py
+def login(db, request):
+    return
+
