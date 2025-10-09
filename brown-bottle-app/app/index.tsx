1c2336aa3e95012a53aacd1afafecf77f5319080
Ishimwe Gentil <igentil295@gmail.com>
2025-10-09 13:44:02 -0500

    fixed login screen on mobile

--- brown-bottle-app/app/index.tsx
+++ brown-bottle-app/app/index.tsx
@@ -1,12 +1,10 @@
-import { View, Text, StatusBar, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import { View, Text, StatusBar, StyleSheet, TouchableWithoutFeedback, Image, Keyboard, Platform } from 'react-native';
 import { useCallback, useState, useContext } from 'react';
 import { useFocusEffect } from '@react-navigation/native';
 
-import { useRouter } from 'expo-router';
 import { Colors } from '@/constants/Colors';
 
 import DefaultView from '@/components/DefaultView';
-import DefaultScrollView from '@/components/DefaultScrollView';
 
 import HandleLogin from '@/components/auth/HandleLogin'
 
@@ -19,27 +17,31 @@ const LandingScreen = () => {
     }, [])
   );
 
+  const dismissKeyboardIfMobile = () => {
+    if (Platform.OS !== 'web') {
+      Keyboard.dismiss();
+    }
+  };
   return (
 
     <DefaultView backgroundColor={Colors.greyWhite}>
+      <TouchableWithoutFeedback onPress={dismissKeyboardIfMobile} >
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: Colors.greyWhite }}>
 
-    
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: Colors.greyWhite }}>
+          <View style={{ alignItems: 'center' }}>
+            <Image
+            source={require('@/assets/images/brown_bottle_logo.jpg')}
+            style={{ width: 150, height: 150, marginTop: 20}}
+            />
+          </View>
 
-        <View style={{ alignItems: 'center' }}>
-          <Image
-          source={require('@/assets/images/brown_bottle_logo.jpg')}
-          style={{ width: 150, height: 150, marginTop: 20}}
-          />
+
+          <View style={{ alignItems: 'center', marginTop: 20 }}>
+            <HandleLogin/> 
+          </View>
+
         </View>
-
-
-        <View style={{ alignItems: 'center', marginTop: 20 }}>
-          <HandleLogin/> 
-        </View>
-
-      </View>
-
+      </TouchableWithoutFeedback>
     </DefaultView>
 
   );
