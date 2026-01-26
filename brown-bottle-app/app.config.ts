216760da11f4b4e7248aa0a81c2a197fc71c2497
Ishimwe Gentil <igentil295@gmail.com>
2026-01-26 13:52:52 -0600

    project id

--- brown-bottle-app/app.config.ts
+++ brown-bottle-app/app.config.ts
@@ -1,45 +1,45 @@
-import 'dotenv/config';
-import { ExpoConfig, ConfigContext } from '@expo/config';
+import "dotenv/config";
+import { ExpoConfig, ConfigContext } from "@expo/config";
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: 'brown-bottle-app',
-  slug: 'brown-bottle-app',
-  version: '1.0.0',
-  orientation: 'portrait',
-  icon: './assets/images/icon.png',
-  scheme: 'myapp',
+  name: "brown-bottle-app",
+  slug: "brown-bottle-app",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/images/icon.png",
+  scheme: "myapp",
 
   newArchEnabled: true,
   ios: {
     supportsTablet: true,
-    userInterfaceStyle: 'light',
+    userInterfaceStyle: "light",
   },
   android: {
-    package: 'com.brownbottle.app',
+    package: "com.brownbottle.app",
     adaptiveIcon: {
-      foregroundImage: './assets/images/adaptive-icon.png',
-      backgroundColor: '#ffffff',
+      foregroundImage: "./assets/images/adaptive-icon.png",
+      backgroundColor: "#ffffff",
     },
   },
   web: {
-    bundler: 'metro',
-    output: 'static',
-    favicon: './assets/images/favicon.png',
+    bundler: "metro",
+    output: "static",
+    favicon: "./assets/images/favicon.png",
   },
   plugins: [
-    'expo-router',
+    "expo-router",
     [
-      'expo-splash-screen',
+      "expo-splash-screen",
       {
-        image: './assets/images/splash-icon.png',
+        image: "./assets/images/splash-icon.png",
         imageWidth: 200,
-        resizeMode: 'contain',
-        backgroundColor: '#ffffff',
+        resizeMode: "contain",
+        backgroundColor: "#ffffff",
       },
     ],
-    'expo-secure-store',
-    'expo-web-browser',
+    "expo-secure-store",
+    "expo-web-browser",
   ],
   experiments: {
     typedRoutes: true,
@@ -47,7 +47,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   extra: {
     // This allows Constants.expoConfig.extra.eas.projectId to work
     eas: {
-      projectId: process.env.EXPO_PUBLIC_EAS_PROJECT_ID ?? "830eaf2f-d229-46b0-8d28-cb5ecdb4b8a4",
+      projectId:
+        process.env.EXPO_PUBLIC_EAS_PROJECT_ID ??
+        "56a5de34-e262-4d71-b648-161a8850cc35",
     },
     // This passes your URL to Constants
     API_BASE_URL: process.env.EXPO_PUBLIC_API_URL,
