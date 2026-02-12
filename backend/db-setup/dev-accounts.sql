f928e6234fcd59f0999000ead8f33d3e25cf8498
1less1 <olhava.dom@gmail.com>
2026-02-12 11:56:21 -0600

    Added dev account script and updated .gitkeep

---
+++ backend/db-setup/dev-accounts.sql
@@ -0,0 +1,16 @@
+USE `thebrownbottle`;
+
+START TRANSACTION;
+
+-- ======================
+-- Employee Data
+-- ======================
+INSERT INTO `thebrownbottle`.`employee` 
+(`first_name`, `last_name`, `email`, `phone_number`, `wage`, `admin`, `primary_role`, `secondary_role`, `tertiary_role`)
+VALUES 
+('Ishimw', 'Gentil', 'gentili@uni.edu', '677-677-6769', 99.50, 1, 1, NULL, NULL),
+('Aaryn', 'Warrior', 'warriora@uni.edu', '999-999-8899', 99.50, 1, 1, NULL, NULL),
+('Dom', 'Olhava', 'olhavad@uni.edu', '699-699-5764', 99.50, 1, 1, NULL, NULL);
+
+
+COMMIT;
