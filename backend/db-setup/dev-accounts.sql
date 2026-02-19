d363f8454b44b7ea228da58b3e320544db4deeec
Ishimwe Gentil <igentil295@gmail.com>
2026-02-19 10:56:54 -0600

    fixed name spelling

--- backend/db-setup/dev-accounts.sql
+++ backend/db-setup/dev-accounts.sql
@@ -8,7 +8,7 @@ START TRANSACTION;
 INSERT INTO `thebrownbottle`.`employee` 
 (`first_name`, `last_name`, `email`, `phone_number`, `wage`, `admin`, `primary_role`, `secondary_role`, `tertiary_role`)
 VALUES 
-('Ishimw', 'Gentil', 'gentili@uni.edu', '677-677-6769', 99.50, 1, 1, NULL, NULL),
+('Ishimwe', 'Gentil', 'gentili@uni.edu', '677-677-6769', 99.50, 1, 1, NULL, NULL),
 ('Aaryn', 'Warrior', 'warriora@uni.edu', '999-999-8899', 99.50, 1, 1, NULL, NULL),
 ('Dom', 'Olhava', 'olhavad@uni.edu', '699-699-5764', 99.50, 1, 1, NULL, NULL);
 
