dc3fb0fb8b88ebf53ae717c826a5adbcffe662c2
Ishimwe Gentil <igentil295@gmail.com>
2026-02-20 15:09:05 -0600

    added berns

--- backend/db-setup/dev-accounts.sql
+++ backend/db-setup/dev-accounts.sql
@@ -10,7 +10,8 @@ INSERT INTO `thebrownbottle`.`employee`
 VALUES 
 ('Ishimwe', 'Gentil', 'gentili@uni.edu', '677-677-6769', 99.50, 1, 1, NULL, NULL),
 ('Aaryn', 'Warrior', 'warriora@uni.edu', '999-999-8899', 99.50, 1, 1, NULL, NULL),
-('Dom', 'Olhava', 'olhavad@uni.edu', '699-699-5764', 99.50, 1, 1, NULL, NULL);
+('Dom', 'Olhava', 'olhavad@uni.edu', '699-699-5764', 99.50, 1, 1, NULL, NULL),
+('Andy', 'Berns', 'andrew.berns@uni.edu', '123-723-5764', 99.50, 1, 1, NULL, NULL);
 
 
 COMMIT;
